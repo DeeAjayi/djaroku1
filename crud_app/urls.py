5354551df7725c54dbb6b1ed17517838d9e814e6
DeeAjayi <ajayidipo@ymail.com>
2017-12-22 14:24:30 +0100

    Added more files

--- crud_app/urls.py
+++ crud_app/urls.py
@@ -7,4 +7,25 @@ urlpatterns = [
     # /students/
     url(r'^$', views.index, name='index'),
 
+    # /students/id<1>
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+
+    # /students/add
+    url(r'add/$', views.StudentCreate.as_view(), name='student-add'),
+
+    # /students/update/<pk>
+    url(r'update/(?P<pk>[0-9]+)/$', views.StudentUpdate.as_view(), name='student-update'),
+
+    # /students/delete/<pk>/
+    url(r'delete/(?P<pk>[0-9]+)/$', views.StudentDelete.as_view(), name='student-delete'),
+
+    # /students/all/
+    url(r'all/$', views.ListView.as_view(), name='all_list'),
+
+    # /students/contact
+    url(r'contact/$', views.index, name='contact'),
+
+    # /students/delete/confirm/
+    url(r'delete/confirm/$', views.delete_confirm, name='delete-confirm'),
+
 ]
--- crud_app/views.py
+++ crud_app/views.py
@@ -1,5 +1,49 @@
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+#from django.views.generic.base import RedirectView
+from django.views import generic
+from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render
+from .models import Student
 
 
 def index(request):
     return render(request, 'crud_app/index.html')
+
+
+def contact(request):
+    return render(request, 'crud_app/contact.html')
+
+
+def delete_confirm(request):
+    return render(request, 'crud_app/student_delete_confirmed.html')
+
+
+class ListView(generic.ListView):
+    template_name = 'crud_app/list_all.html'
+    context_object_name = 'all_students'
+
+    def get_queryset(self):
+        return Student.objects.all()
+
+
+class DetailView(generic.DetailView):
+    model = Student
+    template_name = 'crud_app/detail.html'
+
+
+class StudentCreate(CreateView):
+    model = Student
+    fields = ['Surname', 'Firstname', 'Department', 'Level', 'email', 'age', 'Phone_number']
+    success_url = reverse_lazy('crud_app:all_list')
+
+
+class StudentUpdate(UpdateView):
+    model = Student
+    fields = ['Surname', 'Firstname', 'Department', 'Level', 'email', 'age', 'Phone_number']
+    success_url = reverse_lazy('crud_app:all_list')
+
+
+class StudentDelete(DeleteView):
+    model = Student
+    success_url = reverse_lazy('crud_app:index')
+
