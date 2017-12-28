04d29dd330c339fdb73cceadeb0a4087d79b5b26
DeeAjayi <ajayidipo@ymail.com>
2017-12-28 09:22:16 +0100

    Added uniqueness to email

--- crud_app/models.py
+++ crud_app/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
+
 class Student(models.Model):
     Surname = models.CharField(max_length=100)
     Firstname = models.CharField(max_length=100)
     Department = models.CharField(max_length=100)
     Level = models.IntegerField()
-    email = models.EmailField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
     age = models.IntegerField()
     Phone_number = models.CharField(max_length=15)
 
